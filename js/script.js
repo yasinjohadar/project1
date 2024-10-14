4af53cdb43a9a35e34a1b29c5d0a71c08ac488c0
yasinjohadar <yasinjohadar@gmail.com>
2024-10-14 14:52:35 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let toggle_menu = document.getElementById('toggle-menu');
+
+let ul_navbar = document.getElementById('ul_navbar');
+let mobil_nav = document.getElementById('mobil-nav');
+
+
+function open_close_nav () {
+    ul_navbar.classList.toggle('nav_mobile');}
+// }
+// function open_close_nav1() {
+//     let nav = document.querySelector('.mobil-nav');
+    
+//     if (nav.style.right === '0px') {
+
+//         nav.style.right = '-300px';
+//           // إرجاع النافذة إلى مكانها
+//     } else {
+//         nav.style.right = '0px';     // فتح النافذة
+//     }
+// }
